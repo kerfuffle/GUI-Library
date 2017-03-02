b36d566b72362da395aef6483b2da6043f579aaa
kerfuffle <thepolarofbears@gmail.com>
2017-03-02 13:39:39 -0500

    Initial Commit

---
+++ Code/GUI_Text/Text_Test/src/net/kerfuffle/Utilities/RGB.java
@@ -0,0 +1,27 @@
+package net.kerfuffle.Utilities;
+
+public class RGB {
+
+	private float r, g, b;
+	
+	public RGB(float r, float g, float b)
+	{
+		this.r=r;
+		this.g=g;
+		this.b=b;
+	}
+	
+	public float R()
+	{
+		return r;
+	}
+	public float G()
+	{
+		return g;
+	}
+	public float B()
+	{
+		return b;
+	}
+	
+}
